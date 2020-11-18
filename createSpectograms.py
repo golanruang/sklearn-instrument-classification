e32d8465d307cdf063dd8a42993286f91057ff26
golanruang <guang.rolan@gmail.com>
2020-11-17 18:00:07 -0800

    imported dataset

---
+++ createSpectograms.py
@@ -0,0 +1,19 @@
+import librosa
+import os
+
+for file in os.listdir()
+# 1. Get the file path to an included audio example
+filename = librosa.example('bass_electronic_018-022-100.wav')
+
+
+# 2. Load the audio as a waveform `y`
+#    Store the sampling rate as `sr`
+y, sr = librosa.load(filename)
+
+# 3. Run the default beat tracker
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+
+# 4. Convert the frame indices of beat events into timestamps
+beat_times = librosa.frames_to_time(beat_frames, sr=sr)
